a81bc980af9dc3e4aba54da86934b2344efaf814
AnthonyDem <thony.dem@gmail.com>
2021-01-13 14:33:43 +0300

    added logging

--- app.py
+++ app.py
@@ -5,11 +5,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from config import Config
-from apispec.ext.marshmallow import  MarshmallowPlugin
+from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec import APISpec
 from flask_apispec.extension import FlaskApiSpec
 from schemas import VideoSchema, AuthSchema, UserSchema
 from flask_apispec import use_kwargs, marshal_with
+import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -22,6 +23,23 @@ session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=en
 
 Base = declarative_base()
 Base.query = session.query_property()
+
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+    file_handler = logging.FileHandler('log/apiv1')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+logger = setup_logger()
+
+
 
 jwt = JWTManager(app)
 
@@ -48,8 +66,12 @@ Base.metadata.create_all(bind=engine)
 @jwt_required
 @marshal_with(VideoSchema(many=True))
 def get_videos():
-    user_id = get_jwt_identity()
-    videos = Video.query.filter(Video.user_id == user_id)
+    try:
+        user_id = get_jwt_identity()
+        videos = Video.query.filter(Video.user_id == user_id)
+    except Exception as e:
+        logger.warning(f'user: {user_id} videos - read action falling with error {e}')
+        return {'message': str(e)}, 400
     return videos
 
 
@@ -58,10 +80,14 @@ def get_videos():
 @use_kwargs(VideoSchema)
 @marshal_with(VideoSchema)
 def post_video(**kwargs):
-    user_id = get_jwt_identity()
-    new_video = Video(user_id=user_id, **kwargs)
-    session.add(new_video)
-    session.commit()
+    try:
+        user_id = get_jwt_identity()
+        new_video = Video(user_id=user_id, **kwargs)
+        session.add(new_video)
+        session.commit()
+    except Exception as e:
+        logger.warning(f'user: {user_id} video - create action failed with error {e}')
+        return {'message': str(e)}, 400
     return new_video
 
 
@@ -70,13 +96,17 @@ def post_video(**kwargs):
 @marshal_with(VideoSchema)
 @use_kwargs(VideoSchema)
 def update_video(video_id, **kwargs):
-    user_id = get_jwt_identity()
-    video = Video.query.filter(Video.id == video_id, Video.user_id == user_id).first()
-    if not video:
-        return {'message': 'Error such video doesn\'t exist'}, 400
-    for key, value in kwargs.items():
-        setattr(video, key, value)
-    session.commit()
+    try:
+        user_id = get_jwt_identity()
+        video = Video.query.filter(Video.id == video_id, Video.user_id == user_id).first()
+        if not video:
+            return {'message': 'Error such video doesn\'t exist'}, 400
+        for key, value in kwargs.items():
+            setattr(video, key, value)
+        session.commit()
+    except Exception as e:
+        logger.warning(f'user: {user_id} video: {video_id} - updated failed with error {e}')
+        return {'message': str(e)}, 400
     return video
 
 
@@ -84,12 +114,16 @@ def update_video(video_id, **kwargs):
 @jwt_required
 @marshal_with(VideoSchema)
 def delete_video(video_id):
-    user_id = get_jwt_identity()
-    video = Video.query.filter(Video.id == video_id, Video.user_id == user_id).first()
-    if not video:
-        return {'message': 'Error such video doesn\'t exist'}, 400
-    session.delete(video)
-    session.commit()
+    try:
+        user_id = get_jwt_identity()
+        video = Video.query.filter(Video.id == video_id, Video.user_id == user_id).first()
+        if not video:
+            return {'message': 'Error such video doesn\'t exist'}, 400
+        session.delete(video)
+        session.commit()
+    except Exception as e:
+        logger.warning(f'user: {user_id} video: {video_id} deleting failed with error {e}')
+        return {'message': str(e)}, 400
     return '', 204
 
 
@@ -97,10 +131,13 @@ def delete_video(video_id):
 @marshal_with(AuthSchema)
 @use_kwargs(UserSchema)
 def register(**kwargs):
-    user = User(**kwargs)
-    session.add(user)
-    session.commit()
-    token = user.get_token()
+    try:
+        user = User(**kwargs)
+        session.add(user)
+        session.commit()
+        token = user.get_token()
+    except Exception as e:
+        return {'message': str(e)}, 400
     return {'access_token': token}
 
 
@@ -108,14 +145,28 @@ def register(**kwargs):
 @use_kwargs(UserSchema(only=['email', 'password']))
 @marshal_with(AuthSchema)
 def login(**kwargs):
-    user = User.authenticate(**kwargs)
-    token = user.get_token()
+    try:
+        user = User.authenticate(**kwargs)
+        token = user.get_token()
+    except Exception as e:
+        return {'message': str(e)}, 400
     return {'access_token': token}
 
 
 @app.teardown_appcontext
 def shutdown_session(exception):
     session.remove()
+
+
+@app.errorhandler(422)
+def error_handler(err):
+    headers = err.data.get('headers', None)
+    message = err.data.get('message', ['Invalid request'])
+    logger.warning(f'Invalid input params {message}')
+    if headers:
+        return jsonify({'message': message}), 400, headers
+    else:
+        return jsonify({'message': message}), 400
 
 
 docs.register(get_videos)
